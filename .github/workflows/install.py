6d81221d0e65eec04fe5d8bcbf856795ba7de795
EricLBuehler <ericlbuehler@gmail.com>
2023-12-03 13:46:15 -0500

    Debugging CI

--- .github/workflows/install.py
+++ .github/workflows/install.py
@@ -1,22 +1,21 @@
 import subprocess
 
-print(subprocess.run("uname -a").stdout)
-subprocess.run("sudo apt update -y")
-subprocess.run("sudo apt install libssl-dev -y")
-subprocess.run("sudo apt install pkg-config -y")
+subprocess.run(["sudo", "apt", "update", "-y"])
+subprocess.run(["sudo", "apt", "install", "libssl-dev", "-y"])
+subprocess.run(["sudo", "apt", "install", "pkg-config", "-y"])
 
-first = subprocess.run("sudo find / -name libtorch_cpu.so").stdout.split("\n")
+first = subprocess.run(["sudo", "find", "/", "-name", "libtorch_cpu.so"]).stdout.split("\n")
 
-nvcc_release = subprocess.run("nvcc --version").stdout.split("\n")[3] #Cuda compilation tools, release 11.5, V11.5.119
+nvcc_release = subprocess.run(["nvcc", "--version"]).stdout.split("\n")[3] #Cuda compilation tools, release 11.5, V11.5.119
 nvcc_release = nvcc_release.split("release ")[1] #['Cuda compilation tools, ', '11.5, V11.5.119']
 nvcc_release = float(nvcc_release[1].split(","))
 
 if nvcc_release<=11.8:
-    subprocess.run("pip install torch==2.1.0 torchvision==0.16.0 torchaudio==2.1.0 --index-url https://download.pytorch.org/whl/cu118")
+    subprocess.run(["pip", "install", "torch==2.1.0", "torchvision==0.16.0", "torchaudio==2.1.0", "--index-url", "https://download.pytorch.org/whl/cu118"])
 else:
-    subprocess.run("pip install torch==2.1.0 torchvision==0.16.0 torchaudio==2.1.0 --index-url https://download.pytorch.org/whl/cu121")
+    subprocess.run(["pip", "install", "torch==2.1.0", "torchvision==0.16.0", "torchaudio==2.1.0", "--index-url", "https://download.pytorch.org/whl/cu121"])
 
-after = subprocess.run("sudo find / -name libtorch_cpu.so").stdout.split("\n")
+after = subprocess.run(["sudo", "find", "/", "-name", "libtorch_cpu.so"]).stdout.split("\n")
 different = list(filter(lambda x: x not in first, after))[0]
 
 with open("~/.bashrc", "a") as f:
@@ -24,4 +23,4 @@ with open("~/.bashrc", "a") as f:
     f.write(f"export LD_LIBRARY_PATH={different}:$LD_LIBRARY_PATH")
     f.write("export LIBTORCH_USE_PYTORCH=1")
 
-subprocess.run("source ~/.bashrc")
+subprocess.run(["source", "~/.bashrc"])
