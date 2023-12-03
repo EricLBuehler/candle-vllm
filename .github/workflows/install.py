40a85897657c136f001095a3a8019e93005652a9
EricLBuehler <ericlbuehler@gmail.com>
2023-12-03 13:52:00 -0500

    Debugging CI

--- .github/workflows/install.py
+++ .github/workflows/install.py
@@ -4,12 +4,25 @@ subprocess.run(["sudo", "apt", "update", "-y"])
 subprocess.run(["sudo", "apt", "install", "libssl-dev", "-y"])
 subprocess.run(["sudo", "apt", "install", "pkg-config", "-y"])
 
-first = subprocess.run(["sudo", "find", "/", "-name", "libtorch_cpu.so"]).stdout.split("\n")
+try:
+    import torch
+    works = True
+except:
+    works = False
 
-nvcc_release = subprocess.run(["nvcc", "--version"]).stdout.split("\n")[3] #Cuda compilation tools, release 11.5, V11.5.119
+if works:
+    first = subprocess.run(["sudo", "find", "/", "-name", "libtorch_cpu.so"]).stdout.split("\n")
+else:
+    first = []
+
+nvcc_release = subprocess.run(["nvcc", "--version"])
+assert nvcc_release.returncode == 0
+
+nvcc_release = nvcc_release.stdout.split("\n")[3] #Cuda compilation tools, release 11.5, V11.5.119
 nvcc_release = nvcc_release.split("release ")[1] #['Cuda compilation tools, ', '11.5, V11.5.119']
 nvcc_release = float(nvcc_release[1].split(","))
 
+print(f"Got nvcc version {nvcc_release}")
 if nvcc_release<=11.8:
     subprocess.run(["pip", "install", "torch==2.1.0", "torchvision==0.16.0", "torchaudio==2.1.0", "--index-url", "https://download.pytorch.org/whl/cu118"])
 else:
