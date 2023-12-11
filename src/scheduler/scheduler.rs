9b060c2ec5095041977534d9b61beee966df2eff
EricLBuehler <ericlbuehler@gmail.com>
2023-12-11 15:29:40 -0500

    Prepare for .schedule

--- src/scheduler/scheduler.rs
+++ src/scheduler/scheduler.rs
@@ -1,23 +1,40 @@
-use std::collections::VecDeque;
+use std::collections::{HashMap, VecDeque};
 
-use super::sequence::SequenceGroup;
+use super::sequence::Sequence;
+
+pub struct SchedulerOutput<'a> {
+    running: &'a [Sequence],
+    blocks_to_swap_in: HashMap<usize, usize>,
+    blocks_to_swap_out: HashMap<usize, usize>,
+    blocks_to_copy: HashMap<usize, Vec<usize>>,
+}
 
 pub struct Scheduler {
-    waiting: VecDeque<SequenceGroup>,
-    runing: VecDeque<SequenceGroup>,
-    swapped: VecDeque<SequenceGroup>,
+    waiting: VecDeque<Sequence>,
+    running: VecDeque<Sequence>,
+    swapped: VecDeque<Sequence>,
 }
 
 impl Scheduler {
     pub fn new() -> Self {
         Self {
             waiting: VecDeque::new(),
-            runing: VecDeque::new(),
+            running: VecDeque::new(),
             swapped: VecDeque::new(),
         }
     }
 
-    pub fn add_sequence(&mut self, seq: SequenceGroup) {
+    pub fn add_sequence(&mut self, seq: Sequence) {
         self.waiting.push_back(seq);
+    }
+
+    pub fn schedule(&mut self) -> SchedulerOutput<'_> {
+        todo!()
+    }
+}
+
+impl Scheduler {
+    fn get_running_slice(&self) -> &[Sequence] {
+        &*self.running.make_contiguous()
     }
 }
--- src/scheduler/sequence.rs
+++ src/scheduler/sequence.rs
@@ -1,7 +1,36 @@
-pub struct Sequence {
-    
+use std::cell::RefCell;
+
+pub struct SequenceData {
+    prompt_token_ids: Vec<usize>,
+    output_token_ids: Vec<usize>,
+    cumulative_logprob: f32,
 }
 
-pub struct SequenceGroup {
-    seqs: Sequence,
+impl SequenceData {
+    pub fn new(prompt_token_ids: Vec<usize>) -> Self {
+        Self {
+            prompt_token_ids,
+            output_token_ids: Vec::new(),
+            cumulative_logprob: 0.,
+        }
+    }
+
+    pub fn append_token_id(&mut self, token_id: usize, logprob: f32) {
+        self.output_token_ids.push(token_id);
+        self.cumulative_logprob += logprob;
+    }
+}
+
+pub struct Sequence {
+    data: RefCell<SequenceData>,
+    seq_id: usize,
+}
+
+impl Sequence {
+    pub fn new(prompt_token_ids: Vec<usize>, seq_id: usize) -> Self {
+        Self {
+            data: RefCell::new(SequenceData::new(prompt_token_ids)),
+            seq_id,
+        }
+    }
 }
