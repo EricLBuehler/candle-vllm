12511c9bc39df40f88c8e6a1bff19694aaaa78a0
sigma-andex <77549848+sigma-andex@users.noreply.github.com>
2023-12-17 09:38:43 +0000

    Add map conversion

--- src/scheduler/cache_engine.cc
+++ src/scheduler/cache_engine.cc
@@ -9,21 +9,34 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include <map>
 #include <iostream>
+// #include "candle-vllm/csrc/cache.h" // [TODO] Need to figure out how to include these
 
 void _swap_blocks(Tensor src, Tensor dst, rust::Vec<SwapPair> _src_to_dst)
 {
-    for (auto i : _src_to_dst)
+    std::map<int64_t, int64_t> block_mappings;
+    for (auto mapping : _src_to_dst)
     {
-        // std::cout << i << std::endl;
+        std::pair<int64_t, int64_t> pair = {mapping.k, mapping.v};
+        block_mappings.insert(pair);
     }
+    // swap_blocks(&torch_src_tensor,&torch_src_tensor,&block_mappings);
 }
 
 void _copy_blocks(rust::Vec<Tensor> key_caches, rust::Vec<Tensor> value_caches, rust::Vec<CopyPair> _src_to_dst)
 {
 
-    for (auto s : _src_to_dst)
+   
+    std::map<int64_t, std::vector<int64_t>> block_mappings;
+    for (auto mapping : _src_to_dst)
     {
-        // std::cout << s.k << "->" << s.v << std::endl;
+        std::vector<int64_t> value;
+        for (auto v : mapping.v) {
+            value.push_back(v);
+        }
+        std::pair<int64_t, std::vector<int64_t>> pair = {mapping.k, value};
+        block_mappings.insert(pair);
     }
+    // copy_blocks(&torch_key_caches,&torch_value_caches,&block_mappings);
 }
