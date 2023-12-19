5fe4f667be4ea6f8e3df31aad712d71ff0ec58d9
EricLBuehler <ericlbuehler@gmail.com>
2023-12-19 11:57:18 -0500

    Update build.rs to link py3

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 fn main() {
+    println!("cargo:rustc-link-search=native=");
+    println!("cargo:rustc-link-lib=python3");
     println!("cargo:rustc-link-search=native=/home/ubuntu/candle-vllm/");
     println!("cargo:rustc-link-lib=rustbind");
 }
