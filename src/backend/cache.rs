3d7a6a0fda660f65a0d99b91b5ed3ce98b64f753
Eric Buehler <ericlbuehler@gmail.com>
2023-12-22 12:40:28 +0000

    Write swap_blocks for htod

--- src/backend/cache.rs
+++ src/backend/cache.rs
@@ -50,13 +50,29 @@ pub fn swap_blocks(
                 let dst_offset: u64 = (dst_block_number * block_size_in_bytes).try_into().unwrap();
                 // u8s because we copy by bytes
                 let src_slice: CudaSlice<u8> = unsafe { src_dev.upgrade_device_ptr(src_ptr+src_offset, block_size_in_bytes) };
-                let mut dst_slice = unsafe { src_dev.upgrade_device_ptr(dst_ptr+dst_offset, block_size_in_bytes) };
+                let mut dst_slice = unsafe { dst_dev.upgrade_device_ptr(dst_ptr+dst_offset, block_size_in_bytes) };
                 
                 try_api!(src_dev.dtod_copy(&src_slice, &mut dst_slice));
             }
         }
-        (Device::Cpu, Device::Cuda(_dst_dev)) => {
-            
+        (Device::Cpu, Device::Cuda(dst_dev)) => {
+            let (src_storage, _src_layout) = src.storage_and_layout();
+            let (dst_storage, dst_layout) = dst.storage_and_layout();
+            assert!(matches!(&*src_storage, Storage::Cpu(_)));
+            assert!(matches!(&*dst_storage, Storage::Cuda(_)));
+            let Storage::Cpu(src_storage) = &*src_storage else { unreachable!() };
+            let Storage::Cuda(dst_storage) = &*dst_storage else { unreachable!() };
+            let dst_ptr = dst_storage.as_cuda_slice::<u8>().map_err(APIError::from)?.device_ptr() + TryInto::<u64>::try_into(dst_layout.start_offset()).unwrap();
+            let src_slice = try_api!(src_storage.as_slice());
+
+            for (src_block_number, dst_block_number) in block_mapping {
+                let src_offset = src_block_number * block_size_in_bytes;
+                let dst_offset: u64 = (dst_block_number * block_size_in_bytes).try_into().unwrap();
+                // u8s because we copy by bytes
+                let mut dst_slice: CudaSlice<u8> = unsafe { dst_dev.upgrade_device_ptr(dst_ptr+dst_offset, block_size_in_bytes) };
+                
+                try_api!(dst_dev.htod_sync_copy_into(&src_slice[src_offset..src_offset+block_size_in_bytes], &mut dst_slice));
+            }
         }
         (Device::Cuda(_src_dev), Device::Cpu) => {
             
