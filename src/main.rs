78f184c5ae88a4f53bd743442183f97461f84c9e
Guoqing Bao <topon@outlook.com>
2024-06-20 14:38:26 +0800

    Improve readability

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ use candle_vllm::scheduler::cache_engine::CacheConfig;
 use candle_vllm::scheduler::SchedulerConfig;
 use candle_vllm::{get_model_loader, hub_load_local_safetensors, ModelSelected};
 use clap::Parser;
+const SIZE_IN_MB: usize = 1024 * 1024;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -59,7 +60,11 @@ struct Args {
 
     /// Available GPU memory for kvcache (MB)
     #[arg(long, default_value_t = 4096)]
-    kvcache_mem: usize,
+    kvcache_mem_gpu: usize,
+
+    /// Available CPU memory for kvcache (MB)
+    #[arg(long, default_value_t = 4096)]
+    kvcache_mem_cpu: usize,
 
     /// Record conversation (default false, the client need to record chat history)
     #[arg(long)]
@@ -80,18 +85,26 @@ async fn main() -> Result<(), APIError> {
         _ => loader.download_model(model_id, None, args.hf_token, args.hf_token_path)?,
     };
 
-    let (dtype, dsize) = match args.dtype.as_deref() {
-        Some("f16") => (DType::F16, 2),
-        Some("bf16") => (DType::BF16, 2),
-        Some("f32") => (DType::F32, 4),
+    let dtype = match args.dtype.as_deref() {
+        Some("f16") => DType::F16,
+        Some("bf16") => DType::BF16,
+        Some("f32") => DType::F32,
         Some(dtype) => panic!("Unsupported dtype {dtype}"),
-        None => (DType::BF16, 2),
+        None => DType::BF16,
     };
 
+    let dsize = dtype.size_in_bytes();
     let device = candle_examples::device(args.cpu).unwrap();
     let model = loader.load_model(paths, dtype, device)?;
     let config = model.0.get_model_config();
-    let num_gpu_blocks = args.kvcache_mem * 1024 * 1024
+    let num_gpu_blocks = args.kvcache_mem_gpu * SIZE_IN_MB
+        / dsize
+        / args.block_size
+        / config.get_num_kv_heads()
+        / config.get_head_size()
+        / config.get_num_hidden_layers()
+        / 2;
+    let num_cpu_blocks = args.kvcache_mem_cpu * SIZE_IN_MB
         / dsize
         / args.block_size
         / config.get_num_kv_heads()
@@ -101,7 +114,7 @@ async fn main() -> Result<(), APIError> {
     let cache_config = CacheConfig {
         block_size: args.block_size,
         num_gpu_blocks: Some(num_gpu_blocks),
-        num_cpu_blocks: Some(32),
+        num_cpu_blocks: Some(num_cpu_blocks),
         fully_init: true,
     };
     println!("Cache config {:?}", cache_config);
