e69f669b88924c8ba5cf1a797091cae7ee648652
Guoqing Bao <topon@outlook.com>
2025-07-28 14:03:56 +0800

    Disable Marlin kernels for CUDA ARC < 800 (#212)

--- kernels/src/marlin_matmul_bf16.cu
+++ kernels/src/marlin_matmul_bf16.cu
@@ -1,5 +1,10 @@
+#ifdef NO_MARLIN_KERNEL
+extern "C" void marlin_4bit_bf16(const void* A, const void* B, void* scales, void* zeros, void* g_idx, void* C, int prob_m, int prob_k, 
+                int prob_n, void* workspace, int groupsize, int64_t stream) {}
+#else
 #include "marlin_gptq_cuda_kernel.cuh"
 extern "C" void marlin_4bit_bf16(const void* A, const void* B, void* scales, void* zeros, void* g_idx, void* C, int prob_m, int prob_k, 
                  int prob_n, void* workspace, int groupsize, int64_t stream) {
     marlin_matmul<nv_bfloat16>(A, B, scales, C, prob_m, prob_k, prob_n, workspace, groupsize, stream);
 }
+#endif
