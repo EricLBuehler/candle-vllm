8a678a0db04d948112c1261b9dd44e498d366145
Guoqing Bao <topon@outlook.com>
2025-10-23 08:48:13 +0000

    Check chat error attribute in chat.py

--- examples/chat.py
+++ examples/chat.py
@@ -107,7 +107,7 @@ def chatloop(system_prompt: Optional[str], stream: bool, live: bool,
                               refresh_per_second=frequency,
                               vertical_overflow="visible") as l:
                         for chunk in response:
-                            if chunk.choices[0].error != None:
+                            if hasattr(chunk.choices[0], "error") and chunk.choices[0].error != None:
                                 raise Exception(chunk.choices[0].error)
                             content = chunk.choices[0].delta.content
                             if content:
@@ -118,7 +118,7 @@ def chatloop(system_prompt: Optional[str], stream: bool, live: bool,
   
                 else:
                     for chunk in response:
-                        if chunk.choices[0].error != None:
+                        if hasattr(chunk.choices[0], "error") and chunk.choices[0].error != None:
                             raise Exception(chunk.choices[0].error)
                         content = chunk.choices[0].delta.content
                         if content:
